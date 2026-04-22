8a8119e423f9a0c625b8f31d46cd157111d7cca8
nazmulo <itsnaazmul@gmail.com>
2026-04-22 19:56:31 +0600

    added basic structure

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+import Countries from './components/countries/Countries'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <Countries />
+          
+    </>
+  )
+}
+
+export default App
---
+++ src/components/countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>in the countries</h1>
+        </div>
+    );
+};
+
+export default Countries;
