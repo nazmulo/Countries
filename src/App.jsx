3ea5f3759e9d66bb630b2a82ab52e38e48b85f77
nazmulo <itsnaazmul@gmail.com>
2026-04-22 21:06:50 +0600

    added the json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 
-
+import { Suspense } from 'react';
 import './App.css'
 import Countries from './components/countries/Countries'
 
+
+const countriesPromise = fetch('https://openapi.programming-hero.com/api/all')
+.then(res => res.json())
+
 function App() {
-  
 
   return (
     <>
       
-      <Countries />
+      <Suspense fallback={<p>Nader on the go.....</p>}>
+      <Countries countriesPromise={countriesPromise}/>
+      </Suspense>
           
     </>
-  )
+  );
 }
 
 export default App
